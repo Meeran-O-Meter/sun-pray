c3bb6be190e8394e7687975392d8b4669d2951fb
Meeran-O-Meter <meeran2resource@gmail.com>
2026-03-19 22:29:18 +0400

    Implemented clock in JavaScript, and implemented the location along with designing.

--- script.js
+++ script.js
@@ -19,3 +19,38 @@ window.addEventListener("load", () => {
 
     }
 });
+
+
+function getLocation() {
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        alert("This browser doesn't support geolocation :(");
+    }
+}
+
+function showPosition() {
+    const latidude = position.coords.latidude;
+    const longitude = navigator.getLocation;
+    
+    document.getElementById("address").innerHTML = longitude + latidude;
+    console.log("Latitude: " + latidude + ", Longitude: " + longitude);
+}
+
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      alert("User denied the request for Geolocation.");
+      break;
+    case error.POSITION_UNAVAILABLE:
+      alert("Location information is unavailable.");
+      break;
+    case error.TIMEOUT:
+      alert("The request to get user location timed out.");
+      break;
+    case error.UNKNOWN_ERROR:
+      alert("An unknown error occurred.");
+      break;
+  }
+}
